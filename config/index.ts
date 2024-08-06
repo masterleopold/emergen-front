b2409e8c08f2cb22c07d7f5341fd04930915e295
Yoichiro Hara <hara@hacci.net>
2024-08-06 16:31:51 +0900

    Update index.ts

--- config/index.ts
+++ config/index.ts
@@ -239,4 +239,4 @@ Thought: {{agent_scratchpad}}
 
 export const VAR_REGEX = /\{\{(#[a-zA-Z0-9_-]{1,50}(\.[a-zA-Z_][a-zA-Z0-9_]{0,29}){1,10}#)\}\}/gi
 
-export const TEXT_GENERATION_TIMEOUT_MS = 60000
+export const TEXT_GENERATION_TIMEOUT_MS = 600000
